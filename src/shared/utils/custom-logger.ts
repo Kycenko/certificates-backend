75eca039958b1d789634192905747d9059e0ffb7
Your Name <lagunovich.vladislav2003@gmail.com>
2025-03-25 23:03:26 +0300

    added ELK stack

--- src/shared/utils/custom-logger.ts
+++ src/shared/utils/custom-logger.ts
@@ -1,28 +1,45 @@
 import { Injectable, LoggerService } from '@nestjs/common'
 import { createLogger, format, transports } from 'winston'
 import 'winston-daily-rotate-file'
+import { ElasticsearchTransport } from 'winston-elasticsearch'
 
 @Injectable()
 export class CustomLogger implements LoggerService {
-	private logger = createLogger({
-		level: 'info',
-		format: format.combine(
-			format.timestamp(),
-			format.printf(({ level, message, timestamp }) => {
-				return `${timestamp} [${level.toUpperCase()}] ${message}`
-			})
-		),
-		transports: [
-			new transports.Console(),
-			new transports.DailyRotateFile({
-				filename: 'logs/application-%DATE%.log',
-				datePattern: 'YYYY-MM-DD',
-				zippedArchive: true,
-				maxSize: '20m',
-				maxFiles: '14d'
-			})
-		]
-	})
+	private logger
+
+	constructor() {
+		const esTransport = new ElasticsearchTransport({
+			level: 'info',
+			clientOpts: { node: 'http://elasticsearch:9200' },
+			index: 'nestjs-logs',
+			format: format.combine(format.timestamp(), format.json())
+		})
+
+		this.logger = createLogger({
+			level: 'info',
+			format: format.combine(
+				format.timestamp(),
+				format.printf(({ level, message, timestamp }) => {
+					return `${timestamp} [${level.toUpperCase()}] ${message}`
+				})
+			),
+			transports: [
+				new transports.Console(),
+				new transports.DailyRotateFile({
+					filename: 'logs/application-%DATE%.log',
+					datePattern: 'YYYY-MM-DD',
+					zippedArchive: true,
+					maxSize: '20m',
+					maxFiles: '14d'
+				}),
+				esTransport
+			],
+			exceptionHandlers: [
+				new transports.File({ filename: 'logs/exceptions.log' }),
+				esTransport
+			]
+		})
+	}
 
 	log(message: string, context?: string) {
 		this.logger.info(this.formatMessage(message, context))
